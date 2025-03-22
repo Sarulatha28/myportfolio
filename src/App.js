2de0773c0766c36653756ae04a192bbd08a43693
Sarulatha28 <sarulathav05@gmail.com>
2025-03-22 12:12:03 +0530

    Fixed router error without affecting deployment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-
-import Header from './components/Header';
-import AboutUs from './components/AboutUs';
-import Contact from './components/Contact';
-import Project from './components/Project';
+// src/App.js
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import AboutUs from "./components/AboutUs";
+import Contact from "./components/Contact";
+import Project from "./components/Project";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <AboutUs />
-      <Project />
-      <Contact />
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/projects" element={<Project />} />
+      </Routes>
     </>
   );
 };
